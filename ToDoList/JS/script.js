c7d4f2d708e13d80f8fdf1cc943d9885b6c0074d
Alexander <alesta73@gmail.com>
2025-09-12 02:43:55 +0200

    Added functionality for creating list with date

--- ToDoList/JS/script.js
+++ ToDoList/JS/script.js
@@ -1,34 +1,79 @@
+let ls = document.querySelector(".listSettings");
+let selectedDate = document.querySelector("#dateID");
+document.onload = () => {
+    selectedDate.value = "";
+}
+
 document.querySelector(".sidebar-toggler").addEventListener("click", () => {
     document.querySelector(".sidebar").classList.toggle("collapsed");
 });
 
-document.querySelector(".addtask").addEventListener("click", () => {
-    console.log("test");
-    let div = document.createElement("div");
-    div.classList.add("list1");
-    //console.log(getDate());
-    let completionDate = document.createElement("h2");
-    completionDate.innerHTML = getDate();
 
-    document.querySelector(".list-container").appendChild(div);
+document.querySelector(".addtask").addEventListener("click", () => {
+    dNoneToggler(ls);
+    // console.log("test");
+    // let div = document.createElement("div");
+    //div.classList.add("list1");
+    //console.log(getDate());
+    //let completionDate = document.createElement("h2");
+    //completionDate.innerHTML = getDate();
+
+    //document.querySelector(".list-container").appendChild(div);
 });
+
+
+
+
+
+document.querySelector(".addTaskBtn").addEventListener("click", () => {
+    
+   
+    if (selectedDate.value == "") {
+        console.log("Ange datum")
+        alert("Ange datum");
+    } else {
+        selectedDate = document.querySelector("#dateID").value;
+        listBuilder(selectedDate);
+        console.log(selectedDate.value);
+        selectedDate.value = "";
+        dNoneToggler(ls);
+        // document.querySelector(".listSettings").classList.toggle("d-none");
+    }
+});
+
+// Click event for task button
+// document.querySelector(".taskButton").addEventListener("click", ()=>{
+//     console.log("click");
+//     dNoneToggler(ls);
+
+// });
+
+function dNoneToggler(data) {
+    document.querySelector(data.classList.toggle("d-none"));
+}
+
+
+function listBuilder(data){
+    //skapar container till listan
+    let list = document.createElement("div");
+    list.classList.add("list");
+    //namn på lista
+    let h2 = document.createElement("h2");
+    h2.innerHTML = data;
+    //linje under namn
+    let line = document.createElement("div");
+    line.classList.add("line");
+    list.appendChild(h2);
+    list.appendChild(line);
+    document.querySelector(".list-container").appendChild(list);
+}
+function taskBuilder(){
+    let task = document.createElement("div");
+    task.classList.add("task");
+}
 
 function getDate() {
     const d = new Date();
     const weekdays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
     return weekdays[d.getDay()];
 }
-let selectedDate = document.querySelector("#dateID");
-document.querySelector(".addTaskBtn").addEventListener("click", ()=>{
-    console.log(selectedDate.value);
-    if(selectedDate.value == ""){
-        console.log("Ange datum")
-    }else{
-        
-        document.querySelector(".listSettings").classList.toggle("d-none");
-    }
-});
-
-// function dNoneToggler(data){
-//     document.querySelector(data.classList.toggle("d-none"));
-// }
